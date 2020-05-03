fda8db6e55825af3bb59d27252739de17808eaa0
vaibhav <vaibhav.katore@foxzyme.com>
2020-05-03 22:24:08 +0530

    initial

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {ImageBackground, StyleSheet, Text, View, TextInput, Button, Alert, ColorPropType } from 'react-native';
+const logoImage = require('./assets/img/back3.jpg');
 
 export default function App() {
-  return (
+  return ( 
+ 
+      
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <ImageBackground
+    source={logoImage}
+    style={styles.images}
+    >
+
+      <View style={styles.inside}>
+      <Text style={{margin:30, fontSize:30, color:"white"}}>Sign Up</Text>
+      <TextInput placeholder="Fist name" style={styles.inputStyles}/>
+      <TextInput placeholder="Last Name" style={styles.inputStyles}/>
+      <TextInput placeholder="Email" style={styles.inputStyles}/>
+      <TextInput placeholder="Phone" style={styles.inputStyles}/>
+      <TextInput placeholder="Password" style={styles.inputStyles}/>
+
+     <Text style={styles.buttonStyle} >Submit</Text>
+     </View>
+     </ImageBackground>
     </View>
+
+
   );
 }
 
@@ -13,7 +33,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
+  inside: {
+    alignItems: 'center',
+  
+  },
+  inputStyles:{
+    borderColor:'black', 
+    borderWidth:0.1, 
+    width:300, 
+    padding:10,
+    marginBottom:10,
+    borderRadius: 10,
+    backgroundColor:"white"
+  },
+   buttonStyle:{
+    fontSize:20,
+    marginTop:10,
+    borderRadius:10, 
+    width:200, 
+    padding:10,
+    backgroundColor:"green",
+    textAlign:"center",
+    color:"white"
+   
+  },
+  images: {
+    flex: 1,
+   resizeMode:"cover"
+},
 });
